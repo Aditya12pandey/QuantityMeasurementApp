ec0bd4654a8e2dc4ee130ae15cffbadfb89f4cd2
Aditya Vikram Pandey <adityavikrammvm@gmail.com>
2026-02-23 17:07:18 +0530

    [Aditya Vikram Pandey].Add - Implemented UC2-FeetAndInchesEquality

---
+++ QuantityMeasurementApp.test/TestClass.cs
@@ -0,0 +1,82 @@
+﻿namespace QuantityMeasurementApp.test;
+
+[TestClass]
+public sealed class TestClass
+{
+
+    [TestMethod]
+    public void TestFeetEquality_SameValue()
+    {
+        var f1 = new Feet(1.0);
+        var f2 = new Feet(1.0);
+
+        Assert.IsTrue(f1.Equals(f2));
+    }
+
+    [TestMethod]
+    public void TestFeetEquality_DifferentValue()
+    {
+        var f1 = new Feet(1.0);
+        var f2 = new Feet(2.0);
+
+        Assert.IsFalse(f1.Equals(f2));
+    }
+
+    
+    [TestMethod]
+    public void TestInchesEquality_SameValue()
+    {
+        var i1 = new Inches(1.0);
+        var i2 = new Inches(1.0);
+
+        Assert.IsTrue(i1.Equals(i2));
+    }
+
+    [TestMethod]
+    public void TestInchesEquality_DifferentValue()
+    {
+        var i1 = new Inches(1.0);
+        var i2 = new Inches(2.0);
+
+        Assert.IsFalse(i1.Equals(i2));
+    }
+
+    
+
+    [TestMethod]
+    public void TestEquality_NullComparison()
+    {
+        var f1 = new Feet(1.0);
+
+        Assert.IsFalse(f1.Equals(null));
+    }
+
+    [TestMethod]
+    public void TestEquality_NonNumericInput()
+    {
+        var f1 = new Feet(1.0);
+
+        Assert.IsFalse(f1.Equals("abc"));
+    }
+
+    [TestMethod]
+    public void TestEquality_SameReference()
+    {
+        var f1 = new Feet(1.0);
+
+        Assert.IsTrue(f1.Equals(f1));
+    }
+
+
+    [TestMethod]
+    public void TestCompareFeet()
+    {
+        Assert.IsTrue(QuantityMeasurementApp.CompareFeet(1.0, 1.0));
+    }
+
+    [TestMethod]
+    public void TestCompareInches()
+    {
+        Assert.IsTrue(QuantityMeasurementApp.CompareInches(1.0, 1.0));
+    }
+}
---
+++ QuantityMeasurementApp/QuantityMeasurementApp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using QuantityMeasurementApp;
+
+namespace QuantityMeasurementApp
+{
+    public class QuantityMeasurementApp
+    {
+        
+
+        public static bool CompareFeet(double value1, double value2)
+        {
+            Feet f1 = new Feet(value1);
+            Feet f2 = new Feet(value2);
+
+            return f1.Equals(f2);
+        }
+
+        public static bool CompareInches(double value1, double value2)
+        {
+            Inches i1 = new Inches(value1);
+            Inches i2 = new Inches(value2);
+
+            return i1.Equals(i2);
+        }
+
+        
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Feet Comparison:");
+            Console.WriteLine(CompareFeet(1.0, 1.0));
+
+            Console.WriteLine("Inches Comparison:");
+            Console.WriteLine(CompareInches(1.0, 1.0));
+        }
+    }
+}
